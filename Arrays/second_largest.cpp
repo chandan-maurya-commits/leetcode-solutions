8be72c7941ad3290f4d716667fa08c2e64a0071f
chandan-maurya-comits <cdnmry@gmail.com>
2026-06-17 01:04:05 +0530

    added one solution

---
+++ Arrays/second_largest.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int getSecondLargest(vector<int> &arr) {
+        // code here
+        int largest = INT_MIN;
+        int secondLargest = INT_MIN;
+        
+        for(int num : arr){
+            if(num > largest){
+                secondLargest = largest;
+                largest = num;
+            }else if(num > secondLargest && num < largest ){
+                secondLargest = num;
+            }
+        }
+        return (secondLargest == INT_MIN)? -1 : secondLargest;
+    }
+};
